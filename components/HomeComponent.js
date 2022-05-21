6a740d38375e8f1dbda39cac0a200e96b9dd7593
ajayjohnthattil <ajayjohnthattil@gmail.com>
2022-05-21 22:27:46 +0530

    cleaned the code

--- components/HomeComponent.js
+++ components/HomeComponent.js
@@ -1,18 +1,21 @@
 import React, { useEffect, useState } from 'react';
 import { ActivityIndicator, FlatList, StyleSheet, Text, View , SafeAreaView, TouchableHighlight, Platform} from 'react-native';
 import Icon from 'react-native-vector-icons/Ionicons'
-//import ModalComponent from './ModalComponent';
 
 const HomeComponent = () => {
+
+
+    // useState hooks
+
     const [isLoading, setLoading] = useState(true);
     const [isGradeLoading, setGradeLoading] = useState(true);
-   
-    const [showGrade, setShowGrade] = useState(false);
     const [activeTile, setActiveTile] = useState('');
     const [data, setData] = useState([]);
     const [data2, setData2] = useState([]);
-    const [schoolData, setSchoolData] = useState([]);
   
+
+    // Methods to fetch data from APIs
+
     const getSchools = async () => {
        try {
         const response = await fetch('https://data.cityofnewyork.us/resource/s3k6-pzi2.json');
@@ -26,35 +29,42 @@ const HomeComponent = () => {
     }
   
     const getGrades = async (DBN) => {
-      try {
-        setGradeLoading(true);   
-       const response = await fetch(`https://data.cityofnewyork.us/resource/f9bf-2cp4.json?dbn=${DBN}`);
-       const json = await response.json();
-       console.log(json)
-       // json[0]===undefined? setData2(null) : setData2(json[0]);
-       setData2(json[0]);
-     } catch (error) {
-       console.error(error);
-     } finally {
-       setGradeLoading(false);
-     }
+        setGradeLoading(true);
+        try {
+          
+            const response = await fetch(`https://data.cityofnewyork.us/resource/f9bf-2cp4.json?dbn=${DBN}`);
+            const json = await response.json();
+            console.log(json)
+            setData2(json[0]);
+        } catch (error) {
+            console.error(error);
+        } finally {
+            setGradeLoading(false);
+        }
    }
   
-   
-    
-  
+   // useEffect hooks
+
     useEffect(() => {
       getSchools();
     }, []);
+
+    useEffect(() => {
+        if(activeTile)
+            getGrades(activeTile);
+      }, [activeTile]);
   
+      
+    // Handling Clicks
+
     const showGrades=(school)=>{
-      getGrades(school.dbn);
-        setShowGrade(!showGrade);
+       setData2(null);
+       activeTile===school.dbn?setActiveTile(null):
         setActiveTile(school.dbn);
-        setSchoolData(school);
       
     }
-  
+    // component  JSX
+    
     return (
       <View style={{ flex: 1, padding: 10}}>
         {isLoading ? <ActivityIndicator/> : (
@@ -67,14 +77,9 @@ const HomeComponent = () => {
               style={{padding: 2, borderWidth: 0.2, borderColor:"#ECECEC"}}
               underlayColor="#DDDDDD"
               key={item.key}
-              //onPress={() => this._onPress(item)}
-              onPress={() => showGrades(item)}
-  
+              onPress={() => {showGrades(item);}}
               onShowUnderlay={separators.highlight}
               onHideUnderlay={separators.unhighlight}>
-              {/* <View style={{ backgroundColor: '#eee' ,alignItems:'center'}}>
-                <Text style={{padding:10}}>{item.dbn} : {item.school_name}</Text>
-              </View> */}
               <View style={{flexDirection:'row'}}>
                 <View style={styles.icon}>
                 <Icon
@@ -92,22 +97,15 @@ const HomeComponent = () => {
               </View>
               
             </TouchableHighlight>
-            {!isGradeLoading?
-            <>
             
-            {showGrade && (activeTile===item.dbn) && item!==null && <Text>{data2?.sat_critical_reading_avg_score || '----'}</Text>}
+            
+            {activeTile===item.dbn && <View style={{height:100,backgroundColor:"green"}}><Text style={{fontSize:50}}>{isGradeLoading?<ActivityIndicator/>:data2?.sat_critical_reading_avg_score }</Text></View>}
              
-            </>
-            :null}
+           
             </View>
           )}
           />
         )}
-        {/* <ModalComponent
-                  school={schoolData}
-                  modalVisible={showGrade}
-                  onClose={() => setShowGrade(false)}
-              /> */}
       </View>
     );
 };
@@ -123,21 +121,18 @@ const styles= StyleSheet.create({
       width           : 80,
       height          : 80,
       borderRadius    : 40,
-     // borderWidth     : 0.5,
       withBackground  : true,
       withBorder      : false,
       
     },
     info:{
       flex: 0.95,
-      justifyContent:'center',
-      flexDirection: 'column',
+      justifyContent :'center',
+      flexDirection  : 'column',
     },
     name:{
       justifyContent :'center',
-      //flexWrap       : 'wrap',
-      overflow:'hidden',
-      //textAlign      : 'center',
+      overflow       :'hidden',
       fontWeight     : 'bold',
       fontSize       : 14,
       fontFamily     : undefined,
@@ -150,8 +145,6 @@ const styles= StyleSheet.create({
     location:{
       justifyContent :'center',
       flexWrap       : 'wrap',
-      // textAlign      : 'center',
-      //fontWeight     : 'bold',
       fontSize       : 12,
       fontFamily     : undefined,
       paddingBottom  : 3,
